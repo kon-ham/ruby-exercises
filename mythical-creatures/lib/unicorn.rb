15ccb5542d5acc691bb93e2319ea4353c85184aa
kon-ham <info.konham@gmail.com>
2021-10-27 13:11:14 -0400

    Pass unicorn is silver by default test

--- mythical-creatures/lib/unicorn.rb
+++ mythical-creatures/lib/unicorn.rb
@@ -1,8 +1,13 @@
 class Unicorn
-    attr_reader :name
+    attr_reader :name,
+                :color
 
     def initialize(name)
         @name = name
         @color = 'silver'
+    end
+
+    def silver?
+        @color == 'silver'
     end
 end
