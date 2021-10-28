7f249d7629e8eb5e6512775a1d91275ec9591fea
Kon Ham <info.konham@gmail.com>
2021-10-28 04:09:58 -0400

    Pass wizard is bearded by default

--- mythical-creatures/lib/wizard.rb
+++ mythical-creatures/lib/wizard.rb
@@ -1,7 +1,12 @@
 class Wizard
     attr_reader :name
-    
-    def initialize(name)
+
+    def initialize(name, bearded: true)
         @name = name
+        @bearded = bearded
+    end
+
+    def bearded?
+        @bearded
     end
 end
