e209104a6179760e68a9ac31f045b6cfc9bfec12
Kon Ham <info.konham@gmail.com>
2021-10-29 13:56:03 -0400

    Pass pirate has a job by default and job can be changed

--- mythical-creatures/lib/pirate.rb
+++ mythical-creatures/lib/pirate.rb
@@ -1,7 +1,9 @@
 class Pirate
-    attr_reader :name
-    
-    def initialize(name)
+    attr_reader :name,
+                :job
+
+    def initialize(name, job = 'Scallywag')
         @name = name
+        @job = job
     end
 end
