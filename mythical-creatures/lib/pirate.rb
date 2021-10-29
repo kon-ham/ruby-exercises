eca05f11674607158a5721e5998dbc74798ba3cc
Kon Ham <info.konham@gmail.com>
2021-10-29 13:58:37 -0400

    Create heinouse_act method and pass test assertion pirate is cursed

--- mythical-creatures/lib/pirate.rb
+++ mythical-creatures/lib/pirate.rb
@@ -5,5 +5,15 @@ class Pirate
     def initialize(name, job = 'Scallywag')
         @name = name
         @job = job
+        @heinous_act_counter = 0
+    end
+
+    def cursed?
+        return true if @heinous_act_counter >= 3
+        false
+    end
+
+    def commit_heinous_act
+        @heinous_act_counter += 1
     end
 end
